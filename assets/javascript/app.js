c291383256a805dd6e5dbb66b0f6f260c452461e
Jay Goss <jayspgoss@gmail.com>
2018-10-27 20:49:07 -0400

    scoring issues

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() { 
+  $(document).ready(function() { 
 
 // janky coding to hide div and questions -- i get FOUC using this
 $("#timer").hide();
@@ -49,106 +49,104 @@ function countdown(seconds) {
   tick();
 }
 
-//logic to calculate total score at the end -- score() function is not working currently
+//logic to calculate total score at the end
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
 
 function score() {
-//score question 1
-//   var x = null;
-//   if (x === null) {
-//     unanswered++;
-//   }
-//   else (x != null) {
-//   var x = document.querySelector('input[name = "question1"]:checked').value;
-//   console.log(x);
-//   if (x === "a") {
-//     correct++;
-//   }
-//   // else if (x != "a" && x!= "b" && x != "c") {
-//   //   unanswered++;
-//   // }
-//   else if (x === null) {
-//         unanswered++;
-//       }
-//   else if (x === "b" || x === "c") {
-//     incorrect++;
-//   };
-//   console.log(x);
-// }
+// score question 1
+  // var x = null;
+  // if (x === undefined) {
+  //   unanswered++;
+  // }
+  // else (x != null) {
+  var x = document.querySelector('input[name = "question1"]:checked').value;
+  if (x === "a") {
+    correct++;
+  }
+  // else if (x != "a" && x!= "b" && x != "c") {
+  //   unanswered++;
+  // }
+  else if (x === null) {
+        unanswered++;
+  }
+  else if (x === "b" || x === "c") {
+    incorrect++;
+  };
+  console.log(x);
 
-// //score question 2
-//   var y = document.querySelector('input[name = "question2"]:checked').value;
-//   if (y === "b") {
-//     correct++;
-//   }
-//   else if (y === null) {
-//     unanswered++;
-//   }
-//   else if (y === "a" || y === "c") {
-//     incorrect++;
-//   };
+//score question 2
+  var y = document.querySelector('input[name = "question2"]:checked').value;
+  if (y === "b") {
+    correct++;
+  }
+  else if (y === null) {
+    unanswered++;
+  }
+  else if (y === "a" || y === "c") {
+    incorrect++;
+  };
 
-// //score question 3
-//   var z = document.querySelector('input[name = "question3"]:checked').value;
-//   if (z === "c") {
-//     correct++;
-//   }
-//   else if (z === null) {
-//     unanswered++;
-//   }
-//   else if (z === "b" || z === "a") {
-//     incorrect++;
-//   };
+//score question 3
+  var z = document.querySelector('input[name = "question3"]:checked').value;
+  if (z === "c") {
+    correct++;
+  }
+  else if (z === null) {
+    unanswered++;
+  }
+  else if (z === "b" || z === "a") {
+    incorrect++;
+  };
 
-// //score question 4
-//   var a = document.querySelector('input[name = "question4"]:checked').value;
-//   if (a === "b") {
-//     correct++;
-//   }
-//   else if (a === null) {
-//     unanswered++;
-//   }
-//   else if (a === "a" || a === "c") {
-//     incorrect++;
-//   };
+//score question 4
+  var a = document.querySelector('input[name = "question4"]:checked').value;
+  if (a === "b") {
+    correct++;
+  }
+  else if (a === null) {
+    unanswered++;
+  }
+  else if (a === "a" || a === "c") {
+    incorrect++;
+  };
 
-// //score question 5
-// var b = document.querySelector('input[name = "question5"]:checked').value;
-// if (b === "a") {
-//   correct++;
-// }
-// else if (b === null) {
-//   unanswered++;
-// }
-// else if (b === "b" || b === "c") {
-//   incorrect++;
-// };
+//score question 5
+var b = document.querySelector('input[name = "question5"]:checked').value;
+if (b === "a") {
+  correct++;
+}
+else if (b === null) {
+  unanswered++;
+}
+else if (b === "b" || b === "c") {
+  incorrect++;
+};
 
-// //score question 6
-// var c = document.querySelector('input[name = "question6"]:checked').value;
-// if (c === "c") {
-//   correct++;
-// }
-// else if (c === null) {
-//   unanswered++;
-// }
-// else if (c === "a" || c === "b") {
-//   incorrect++;
-// };
+//score question 6
+var c = document.querySelector('input[name = "question6"]:checked').value;
+if (c === "c") {
+  correct++;
+}
+else if (c === null) {
+  unanswered++;
+}
+else if (c === "a" || c === "b") {
+  incorrect++;
+};
 
-// //score question 7
-// var d = document.querySelector('input[name = "question7"]:checked').value;
-// if (d === "c") {
-//   correct++;
-// }
-// else if (d === null) {
-//   unanswered++;
-// }
-// else if (d === "a" || d === "b") {
-//   incorrect++;
-// };
+//score question 7
+var d = document.querySelector('input[name = "question7"]:checked').value;
+if (d === "c") {
+  correct++;
+}
+else if (d === null) {
+  unanswered++;
+}
+else if (d === "a" || d === "b") {
+  incorrect++;
+};
 }
 
 //display the score in the divs
